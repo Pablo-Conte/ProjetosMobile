938e4a32acba9ad0980fb48fe54f8cca6d2748ac
Pablo Conte Correa <pablo.correa.nr@gmail.com>
2022-09-15 11:15:23 -0300

    Feat -> Subtração

--- AplicativoExtraCalculadora/app/src/main/java/com/example/aplicativo5_subtrair/MainActivity.kt
+++ AplicativoExtraCalculadora/app/src/main/java/com/example/aplicativo5_subtrair/MainActivity.kt
@@ -22,6 +22,42 @@ class MainActivity : AppCompatActivity() {
         var tvResultado = findViewById(R.id.tvResultado) as TextView
         var errorMessage: String = ""
 
+        var btn1 = findViewById(R.id.btn1) as Button
+        var btn2 = findViewById(R.id.btn2) as Button
+        var btn3 = findViewById(R.id.btn3) as Button
+        var btn4 = findViewById(R.id.btn4) as Button
+        var btn5 = findViewById(R.id.btn5) as Button
+        var btn6 = findViewById(R.id.btn6) as Button
+        var btn7 = findViewById(R.id.btn7) as Button
+        var btn8 = findViewById(R.id.btn8) as Button
+        var btn9 = findViewById(R.id.btn9) as Button
+
+        //Função para colocar números no edit text em focus: putNumber()
+        //1 -> Passar o btn@ como parametro
+        //2 -> Pegar esse btn e verificar qual dos botões de 1..9 é para jogar dentro do edt em focus
+        //3 -> Passar o número para dentro do edt
+
+        fun putNumber(numero: String){
+            if (edtOperando1.isFocused) {
+                var edt: String = edtOperando1.text.toString() + numero
+                if (edtOperando1.text.isNotEmpty()) {
+                    edtOperando1.setText(edt)
+                } else {
+                    edtOperando1.setText(numero)
+                }
+
+            }
+            if (edtOperando2.isFocused){
+                var edt = edtOperando2.text.toString() + numero
+                if (edtOperando2.text.isNotEmpty()) {
+                    edtOperando2.setText(edt)
+                } else {
+                    edtOperando2.setText(numero)
+                }
+
+            }
+        }
+
         btnLimpar.isEnabled = false
 
         btnCalcular.setOnClickListener() {
@@ -52,9 +88,19 @@ class MainActivity : AppCompatActivity() {
             edtOperando1.text.clear()
             edtOperando2.text.clear()
             edtOperando1.requestFocus()
-            tvResultado.setText("Resultado da Operação")
             btnLimpar.isEnabled = false
         }
+
+
+        btn1.setOnClickListener(){ putNumber("1") }
+        btn2.setOnClickListener(){ putNumber("2") }
+        btn3.setOnClickListener(){ putNumber("3") }
+        btn4.setOnClickListener(){ putNumber("4") }
+        btn5.setOnClickListener(){ putNumber("5") }
+        btn6.setOnClickListener(){ putNumber("6") }
+        btn7.setOnClickListener(){ putNumber("7") }
+        btn8.setOnClickListener(){ putNumber("8") }
+        btn9.setOnClickListener(){ putNumber("9") }
 
     } //fim do create
 } //fim do main
