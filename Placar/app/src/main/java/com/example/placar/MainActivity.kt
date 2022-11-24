a72b6e88d5f3e88c50ae552cdaa6d3f276c461a2
Pablo Conte Correa <pablo.correa.nr@gmail.com>
2022-11-24 10:22:02 -0300

    Feat -> New app, Score to volley!

--- Placar/app/src/main/java/com/example/placar/MainActivity.kt
+++ Placar/app/src/main/java/com/example/placar/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.placar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +10,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var txtvPontosTime1 = findViewById(R.id.btnPontosTime1) as TextView
+        var txtvPontosTime2 = findViewById(R.id.btnPontosTime2) as TextView
+        var btnMaisTime1 = findViewById(R.id.btnMaisTime1) as Button
+        var btnMaisTime2 = findViewById(R.id.btnMaisTime2) as Button
 
+        fun acrescentarPontos(time: TextView) {
 
+            var numeroPontosTime = time.text.toString().toInt()
+            numeroPontosTime = numeroPontosTime + 1
+            time.text = numeroPontosTime.toString()
+        }
 
-
-
+        btnMaisTime1.setOnClickListener() { acrescentarPontos(txtvPontosTime1) }
+        btnMaisTime2.setOnClickListener() { acrescentarPontos(txtvPontosTime2) }
 
     }
 }
